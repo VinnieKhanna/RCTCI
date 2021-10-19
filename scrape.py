ccca0132d1bf0a21508845d86e247c0fcce6dbe2
Rohit <rdas49@gatech.edu>
2021-10-18 23:12:45 -0400

    Made it cooler

--- scrape.py
+++ scrape.py
@@ -4,7 +4,7 @@ import pandas as pd
 """ You may need to use pip install pyppdf if
 you get an error asking to install Chromium
 """
-import pyppdf.patch_pyppeteer
+# import pyppdf.patch_pyppeteer
 
 ### Data Collection
 
@@ -14,7 +14,7 @@ import json
 
 htmlText = requests.get('https://leetcode.com/api/problems/algorithms/').text
 problemData = json.loads(htmlText)
-problems = problemData['stat_status_pairs']
+problems = problemData['stat_status_pairs'][0:2]
 cleaned_problems = []
 for problem in problems:
   cleaned_problem = {}
@@ -27,6 +27,8 @@ for problem in problems:
 df = pd.DataFrame(reversed(cleaned_problems))
 df.set_index('id', inplace=True)
 
+
+
 # import HTMLSession from requests_html
 from requests_html import HTMLSession
 session = HTMLSession()
@@ -35,12 +37,21 @@ session = HTMLSession()
 def get_and_render(row):
   r = session.get('https://leetcode.com/problems/' + row['title_slug'])
   r.html.render()
-  time.sleep(1)
-  print(r) # make sure this is never 429
-  return r.html.html
+  html = str(r.html.html)
+
+  #TODO: This is where I am string parsing someone help
+  html = html[html.find('content__u3I1 question-content__JfgR'):]
+  problem = html[html.find('div'): html.find('/div')]
+
+  # print(r.html.html) # make sure this is never 429
+  # raise Exception('Paused')
+  return problem
 
 ##contains rendered html for each page, now parse problem descriptions from each respective html
 df['description'] = df.apply(lambda x: get_and_render(x), axis=1)
-
-# ### Excel Output
-# df.to_csv("problem_data.csv")
+# print(df)
+# print(df.keys())
+### Excel Output
+df.to_csv("problem_data.csv")
+print("Problem:")
+print(df.iloc[0]['description'][0:1000])
