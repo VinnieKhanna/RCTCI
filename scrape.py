d314d2a2e102e49202a4d59c10b6080e6ba41a83
Rohit <rdas49@gatech.edu>
2021-10-25 00:29:52 -0400

    Cool stuff

--- scrape.py
+++ scrape.py
@@ -11,12 +11,15 @@ you get an error asking to install Chromium
 import time
 import requests
 import json
+from tqdm import tqdm
 
 htmlText = requests.get('https://leetcode.com/api/problems/algorithms/').text
 problemData = json.loads(htmlText)
-problems = problemData['stat_status_pairs'][0:2]
+problems = problemData['stat_status_pairs'][0:50]
 cleaned_problems = []
-for problem in problems:
+
+print("Progress for making problems:")
+for problem in tqdm(problems):
   cleaned_problem = {}
   cleaned_problem['id'] = problem['stat']['question_id']
   cleaned_problem['title'] = problem['stat']['question__title']
@@ -36,22 +39,44 @@ session = HTMLSession()
 
 def get_and_render(row):
   r = session.get('https://leetcode.com/problems/' + row['title_slug'])
+  file = open('r.txt', 'w', encoding='utf-8')
+  file.write(r.text)
+  file.close() 
   r.html.render()
   html = str(r.html.html)
-
+  # print(row['title_slug'])
   #TODO: This is where I am string parsing someone help
-  html = html[html.find('content__u3I1 question-content__JfgR'):]
+  # html2 = html[html.find('question-content'):]
+  print(html.find('content__u3I1 question-content'))
+  file = open('output.txt', 'w', encoding='utf-8')
+  file.write(html)
+  file.close()
+  # raise Exception()
+  html = html[html.find('content__u3I1 question-content'):]
+  # print(f"Test: {html == html2}")
+  # print(html)
+  # print(f"first index: {html.find('div')}")
+  # print(f"second index: {html.find('/div')}")
+  if html.find('div') == -1 or html.find('/div') == -1:
+    print("Not found")
+    
+    print(html)
+    raise Exception("Pausing")
   problem = html[html.find('div'): html.find('/div')]
+
 
   # print(r.html.html) # make sure this is never 429
   # raise Exception('Paused')
   return problem
 
 ##contains rendered html for each page, now parse problem descriptions from each respective html
-df['description'] = df.apply(lambda x: get_and_render(x), axis=1)
+print("HTML Progress:")
+tqdm.pandas()
+df['description'] = df.progress_apply(lambda x: get_and_render(x), axis=1)
 # print(df)
 # print(df.keys())
 ### Excel Output
 df.to_csv("problem_data.csv")
 print("Problem:")
 print(df.iloc[0]['description'][0:1000])
+
