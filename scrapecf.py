3d99bc7abda00944fb016ffb0ab6a445101ba035
Pranay <pranay.agra@gmail.com>
2021-10-26 14:33:02 -0400

    codeforces data scraped, text not cleaned

--- scrapecf.py
+++ scrapecf.py
@@ -33,11 +33,6 @@ import re
 import operator
 import csv
 
-tqdm.pandas()
-
-all_runtimes = {}
-problemID = 0
-
 def getWebsite(page):
   driver.get(f'https://codeforces.com/problemset/page/{page}?order=BY_RATING_ASC/')
   delay = 20
@@ -48,18 +43,32 @@ def getWebsite(page):
   except TimeoutException:
     print("Loading took too much time!")
     return "<timed out, manual entry>"
-  time.sleep(5)
+  time.sleep(2)
   html = driver.page_source
   problems = html.split('<tbody>')[1].split("<tr>")[2:]
 
   print(len(problems))
-  if len(problems) < 10:
+  if page <= 71 and len(problems) != 101:
     print(f"PAGE {page} took too long...")
     time.sleep(5)
     html = driver.page_source
     problems = html.split('<tbody>')[1].split("<tr>")[2:]
 
   return problems
+
+def getProblemDescriptionWebsite(website):
+  driver.get(website)
+  delay = 20
+  try:
+    WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'input-specification')))
+    # WAIT TILL CONTENT IS LOADED
+    print("Page is ready!")
+  except TimeoutException:
+    print("Loading took too much time!")
+    return "<timed out, manual entry>"
+  time.sleep(1)
+  html = driver.page_source
+  return html
 
 def get_problem_info(problem):
   data = problem.split("<td")
@@ -69,7 +78,7 @@ def get_problem_info(problem):
   try:
     title = data[2].split('<a')[1].split(">")[1].split("</a")[0].strip()
   except:
-    return None, None, None
+    return None, None, None, None
 
   # get tags
   tags = data[2].split("tags=")
@@ -80,49 +89,52 @@ def get_problem_info(problem):
   # get difficulty
   difficulty = data[4].split("ProblemRating\">")[1].split("<")[0]
 
-  # get website
-  website = None
+  # get text
+  text = None
   try:
-    website = data[2].split('<a href=\"')[1].split(">")[0].strip()
-    print(website)
-  input()
+    website = 'https://codeforces.com/' + data[2].split('<a href=\"')[1].split("\">")[0].strip()
+    html = getProblemDescriptionWebsite(website)
+    description = html.split('problem-statement')[2].split("$(function")[0].strip().split("<p>")[1:]
+    for idx, desc in enumerate(description):
+      if 'sample-tests' in desc:
+        description = description[:idx]
+        break
+    description = '<p>'.join(description).split('input-specification')[0]
+    description = description.replace('\n','')
+    # print(description)
+  except:
+    return None, None, None, None
 
-  return title, tags, difficulty
-
-#input-specification
+  return description, title, tags, difficulty
 
 def get_and_render_discussions(): 
   problemID = 0
 
   data = []
-
-  for page in range(1, 73): #73
+  for page in tqdm(range(1, 73)): #73
     time.sleep(3)
     problems = getWebsite(page)
     for problem in problems:
-      title, tags, difficulty = get_problem_info(problem)
-      if title:
+      description, title, tags, difficulty = get_problem_info(problem)
+      if description and title:
         title = title.encode("ascii", "ignore")
         title = title.decode()
-        # print(title)
+        description = description.encode("ascii", "ignore")
+        description = description.decode()
+        description = description
         problemID += 1
-        data.append({'id': problemID, 'title': title, 'tags': tags, 'difficulty': difficulty})
+        data.append({'id': problemID, 'title': title, 'tags': tags, 'difficulty': difficulty, 'description': description})
+        # print(data)
 
-  # with open('cf_metadata.txt', 'r') as fp:
-  #   line = fp.read()
-  #   line = line.decode('utf-8','ignore').encode("utf-8")
-  #   data = json.load(line, encoding="utf8")
+  # print(data)
 
-  print(data)
-
-  csv_columns = ['id', 'title', 'tags', 'difficulty']
-  csv_file = "problem_data_cf.csv"
+  csv_columns = ['id', 'title', 'tags', 'difficulty', 'description']
+  csv_file = "problem_data_cf2.csv"
   try:
     with open(csv_file, 'w', newline='', encoding="utf-8") as csvfile:
       writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
       writer.writeheader()
       for d in data:
-        print(d)
         writer.writerow(d)
   except IOError:
     print("I/O error")
